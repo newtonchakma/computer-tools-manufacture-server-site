b14df14f931a836dac14792ea031bf4d513dfa8a
newton chakma <newtonchakma304@gmail.com>
2022-06-20 13:05:14 +0600

    connect mongodb and set and get api

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { query } = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vfcr5oq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+        try{
+            await client.connect()
+            console.log('database connected');
+            const serviceCollection = client.db('computer_tool').collection('services');
+
+            app.get('/service', async(req, res) =>{
+                const query = {};
+                const cursor = serviceCollection.find(query);
+                const services = await cursor.toArray()
+                res.send(services)
+
+            })
+        }
+        finally{
+
+        }
+}
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Computer tools app listening on port ${port}`)
+})
