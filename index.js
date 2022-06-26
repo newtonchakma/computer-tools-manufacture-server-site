973f805ef4bb20362b2fa7a81d1974a2fe760666
newton chakma <newtonchakma304@gmail.com>
2022-06-26 17:10:14 +0600

    error test

--- index.js
+++ index.js
@@ -190,12 +190,12 @@ async function run(){
       res.send(result);
     })
 
-    /* app.get("/orders/:id", async (req, res) => {
+    app.get("/orders/:id", async (req, res) => {
       const id = req.params.id
       const query = { _id: ObjectId(id) }
       const result = await orderCollection.findOne(query)
-      res.send({ success: "get successfully", result })
-  }) */
+      res.send( result )
+  }) 
     
 
         }
