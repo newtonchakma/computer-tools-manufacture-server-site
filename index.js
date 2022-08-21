390c2bcbb9c9683b0ec7bec992efe8df93a0c1d8
newton chakma <newtonchakma304@gmail.com>
2022-08-21 14:52:35 +0600

    payment api added

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { query } = require('express');
-const jwt = require('jsonwebtoken');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const { query } = require('express');
 
 
-app.use(cors());
+
+
+
 app.use(express.json());
+app.use(cors());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vfcr5oq.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-function varifyJWT(req,res,next){
+// ----------------verify JWT START---------------//
+function verifyJWT(req,res,next){
   const authHeader = req.headers.authorization;
   if(!authHeader){
     return res.status(401).send({message:'UnAuthorized access'});
@@ -26,22 +30,26 @@ function varifyJWT(req,res,next){
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
     if(err){
-      return res.status(403).send({message: 'Forbidden access'})
+      return res.status(403).send({message: 'Forbidden access'});
     }
     req.decoded = decoded;
     next();
-  })
-}
+  });
+} 
+
+// -------------------VERIFY END-----------------//
 
 async function run(){
         try{
             await client.connect()
-            console.log('database connected');
+            console.log('database connected hello');
             const serviceCollection = client.db('computer_tool').collection('services');
             const orderCollection = client.db('computer_tool').collection('orders');
             const reviewCollection = client.db('computer_tool').collection('reviews');
             const usersCollection = client.db('computer_tool').collection('users');
             const profileCollection = client.db('computer_tool').collection('profile');
+            const paymentCollection = client.db('computer_tool').collection('payments');
+
 
             app.get('/service', async(req, res) =>{
                 const query = {};
@@ -72,20 +80,24 @@ async function run(){
           res.send(result)
         })
         
-       app.post('/create-payment-intent',  async(req,res)=>{
-        const service =req.body;
-        const price = service.price;
+
+      
+       app.post('/create-payment-intent', async(req, res) =>{
+        const service = req.body;
+        console.log('payment-order', service);
+        const price = service.totalPrice;//price
+        console.log('helllll', price);
         const amount = price*100;
         const paymentIntent = await stripe.paymentIntents.create({
-          amount:amount,
-          currency:'usd',
+          amount : amount,
+          currency: 'usd',
           payment_method_types:['card']
         });
         res.send({clientSecret: paymentIntent.client_secret})
-       });
+      });
 
-      //create admine
-      app.put('/user/admin/:email',varifyJWT, async(req,res)=>{
+      //create admine/,,
+      app.put('/user/admin/:email',verifyJWT, async(req,res)=>{
         const email=req.params.email;
         const requester = req.decoded.email;
         const requesterAccoount = await usersCollection.findOne({email:requester});
@@ -123,8 +135,8 @@ async function run(){
        const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
        res.send({result, token});
       })
-      // get user
-      app.get('/user',varifyJWT, async(req,res)=>{
+      // get user/verifyJWT
+      app.get('/user',verifyJWT, async(req,res)=>{
         const users = await usersCollection.find().toArray()
         res.send(users)
       })
@@ -140,8 +152,8 @@ async function run(){
           const results = await orderCollection.insertOne(orderEmail);
           res.send({success:true,results})
         })
-        //get order
-        app.get('/orders',varifyJWT, async(req,res)=>{
+        //get order/verifyJWT,
+        app.get('/orders',verifyJWT, async(req,res)=>{
           const orderEmail =req?.query?.orderEmail;
           const decodedEmail = req.decoded.email;
           if(orderEmail === decodedEmail){
@@ -149,9 +161,9 @@ async function run(){
             const order =await orderCollection.find(query).toArray();
            return res.send(order)
           }
-          /* else{
+           else{
             return res.status(403).send({message: 'forbidden access'})
-          } */
+          }  
          
         })
 
@@ -190,13 +202,36 @@ async function run(){
       res.send(result);
     })
 
-  /*   app.get("/orders/:id", async (req, res) => {
+     /* app.get("/orders/:id",verifyJWT, async (req, res) => {
       const id = req.params.id
       const query = { _id: ObjectId(id) }
       const result = await orderCollection.findOne(query)
       res.send( result )
-  })  */
-    
+  })    */
+
+  app.get('/orders/:id', async(req,res)=>{
+    const id = req.params.id;
+    console.log('hdjhfdjf', id);
+    const query ={_id:ObjectId(id)};
+    const result = await orderCollection.findOne(query);
+    res.send(result);
+  })
+    app.patch("/orders/:id",verifyJWT, async (req,res)=>{
+       const id =req.params.id;
+       const payment =req.body;
+       const filter = {_id:ObjectId(id)};
+       const updateDoc ={
+        $set:{
+          paid:true,
+          transactionId: payment.transactionId
+
+        }
+       }
+
+       const result = await paymentCollection.insertOne(payment);
+       const updatedOrder = await orderCollection.updateOne(filter,updateDoc);
+       res.send(updatedOrder)
+    })
 
         }
         finally{
